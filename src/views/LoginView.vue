301fafa462726c329f3c66b14a4ec19e427742e8
CMaximuss <clan141@gmail.com>
2024-06-16 19:47:30 -0300

    Login v1

---
+++ src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+  
+
+    <div class="containerLogin">
+      <div class="izquierda"></div>
+      <div class="derecha">
+  
+          <h1 class="neon">Dr Strange</h1>
+          <div class="formulario center">
+              
+              <form @submit.prevent="loguear" class="center" >
+  
+                  <label for="email">Correo</label>
+                  <input type="text" v-model="usuario.email">
+                  <label for="password">Contraseña</label>
+                  <input type="password" v-model="usuario.password">
+  
+                  <button type="submit" @click="loguear" class="loginButton">Ingresar</button>
+  
+              </form>
+  
+  
+          </div>
+     </div>
+  
+    </div>
+  </template>
+
+  
